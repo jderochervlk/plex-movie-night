f30fe0c88a20256ccd959b698c933da161938208
jderochervlk <josh@vlkpack.com>
2025-03-16 01:06:54 -0400

    just use tsx for app

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,7 +3,7 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $_404 from "./routes/_404.mjs";
-import * as $_app from "./routes/_app.mjs";
+import * as $_app from "./routes/_app.tsx";
 import * as $api_login from "./routes/api/login.mjs";
 import * as $api_name from "./routes/api/name.mjs";
 import * as $index from "./routes/index.mjs";
@@ -16,7 +16,7 @@ import type { Manifest } from "$fresh/server.ts";
 const manifest = {
   routes: {
     "./routes/_404.mjs": $_404,
-    "./routes/_app.mjs": $_app,
+    "./routes/_app.tsx": $_app,
     "./routes/api/login.mjs": $api_login,
     "./routes/api/name.mjs": $api_name,
     "./routes/index.mjs": $index,
---
+++ routes/_app.tsx
@@ -0,0 +1,27 @@
+import { PageProps } from "$fresh/server.ts";
+
+export default function ({ Component }: PageProps) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex" />
+        <title>Movie Night</title>
+        <link rel="stylesheet" href="/styles.css" />
+      </head>
+      <body class="bg-slate-800 text-slate-50">
+        <header class="py-2 mx-auto bg-blue-900">
+          <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+            <a href="/">
+              <h1 class="text-xl font-bold">Movie Night</h1>
+            </a>
+          </div>
+        </header>
+        <main className="max-width=[1700px] m-auto p-4 text-lg ">
+          <Component />
+        </main>
+      </body>
+    </html>
+  );
+}
