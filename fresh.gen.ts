03d7e65ab0796604829cc6e6b8a9ea3e98b17807
jderochervlk <josh@vlkpack.com>
2025-03-15 22:37:16 -0400

    I think it's working now

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,27 +2,27 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $_404 from "./routes/_404.js";
-import * as $_app from "./routes/_app.js";
-import * as $api_login from "./routes/api/login.js";
-import * as $api_name from "./routes/api/name.js";
-import * as $index from "./routes/index.js";
-import * as $movie_ratingKey_ from "./routes/movie/[ratingKey].js";
-import * as $setname from "./routes/setname.js";
-import * as $signin from "./routes/signin.js";
+import * as $_404 from "./routes/_404.mjs";
+import * as $_app from "./routes/_app.mjs";
+import * as $api_login from "./routes/api/login.mjs";
+import * as $api_name from "./routes/api/name.mjs";
+import * as $index from "./routes/index.mjs";
+import * as $movie_ratingKey_ from "./routes/movie/[ratingKey].mjs";
+import * as $setname from "./routes/setname.mjs";
+import * as $signin from "./routes/signin.mjs";
 
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_404.js": $_404,
-    "./routes/_app.js": $_app,
-    "./routes/api/login.js": $api_login,
-    "./routes/api/name.js": $api_name,
-    "./routes/index.js": $index,
-    "./routes/movie/[ratingKey].js": $movie_ratingKey_,
-    "./routes/setname.js": $setname,
-    "./routes/signin.js": $signin,
+    "./routes/_404.mjs": $_404,
+    "./routes/_app.mjs": $_app,
+    "./routes/api/login.mjs": $api_login,
+    "./routes/api/name.mjs": $api_name,
+    "./routes/index.mjs": $index,
+    "./routes/movie/[ratingKey].mjs": $movie_ratingKey_,
+    "./routes/setname.mjs": $setname,
+    "./routes/signin.mjs": $signin,
   },
   islands: {},
   baseUrl: import.meta.url,
