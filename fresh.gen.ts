1ddfe947a278503e5e3903d8f4c8136cef94f9d5
jderochervlk <josh@vlkpack.com>
2025-03-30 17:39:31 -0400

    add partial to search page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,13 +2,33 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
+import * as $_404 from "./routes/_404.mjs";
 import * as $_app from "./routes/_app.tsx";
+import * as $api_login from "./routes/api/login.mjs";
+import * as $api_name from "./routes/api/name.mjs";
+import * as $api_thumb_title_ from "./routes/api/thumb/[title].mjs";
+import * as $index from "./routes/index.mjs";
+import * as $movie_ratingKey_ from "./routes/movie/[ratingKey].mjs";
+import * as $search from "./routes/search.mjs";
+import * as $setname from "./routes/setname.mjs";
+import * as $signin from "./routes/signin.mjs";
+import * as $watch from "./routes/watch.mjs";
 
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
+    "./routes/_404.mjs": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/api/login.mjs": $api_login,
+    "./routes/api/name.mjs": $api_name,
+    "./routes/api/thumb/[title].mjs": $api_thumb_title_,
+    "./routes/index.mjs": $index,
+    "./routes/movie/[ratingKey].mjs": $movie_ratingKey_,
+    "./routes/search.mjs": $search,
+    "./routes/setname.mjs": $setname,
+    "./routes/signin.mjs": $signin,
+    "./routes/watch.mjs": $watch,
   },
   islands: {},
   baseUrl: import.meta.url,
