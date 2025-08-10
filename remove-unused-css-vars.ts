3b8b0f895e68ea5072e538e9a6be0f4b10660850
Josh Vlk <josh@vlkpack.com>
2025-08-10 10:36:24 -0400

    cleanup unused files and use session storage instead of local

--- remove-unused-css-vars.ts
+++
@@ -1,8 +0,0 @@
-import { bundle } from "jsr:@jotsr/smart-css-bundler";
-
-bundle(["./static/styles.css"], {
-  bundleDir: "./css",
-  assetDir: "./static",
-  cacheDir: "./cache",
-  dev: false,
-});
