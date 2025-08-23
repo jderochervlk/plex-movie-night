4e9e3664f252400dd3851227463c1db706219e38
Josh Vlk <josh@vlkpack.com>
2025-08-23 08:18:16 -0400

    fix: have scroll restore attached to page

--- islands/Scroll.tsx
+++ islands/Scroll.tsx
@@ -2,11 +2,16 @@ import { useEffect } from "preact/hooks";
 
 export const Scroll = () => {
   useEffect(() => {
-    const scrollPosition = Number(sessionStorage.getItem("scroll-position")) ||
+    const page = globalThis.location.pathname;
+    const scrollPosition =
+      Number(sessionStorage.getItem(`scroll-position-${page}`)) ||
       0;
     globalThis.scrollTo(0, scrollPosition);
     return () => {
-      sessionStorage.setItem("scroll-position", globalThis.scrollY.toString());
+      sessionStorage.setItem(
+        `scroll-position-${page}`,
+        globalThis.scrollY.toString(),
+      );
     };
   }, []);
   return null;
